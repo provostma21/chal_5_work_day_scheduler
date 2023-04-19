e0ddc62e76309120f472491937a100c0bd79a107
Matthew M. Provost <provostmat@gmail.com>
2023-04-19 15:16:50 -0400

    added for-loop to cycle through the time-block divs and add class based on current time comparison

--- script.js
+++ script.js
@@ -1,36 +1,63 @@
-var saveButtonEl= $('#saveBtn');
-var dateDisplayEl= $('<header>');
+var saveButtonEl= $('.btn saveBtn');
+var dateDisplayEl= $('#currentDay');
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  saveButtonEl.on('click', function () {
 
-  })
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  // Function to create and format the current date to display at top of page.
   function displayDate() {
-    var currentDate = dayjs().format('MMM DD, YYYY');
+    var currentDate = dayjs().format('MMM DD, YYYY; h:mm A');
     dateDisplayEl.text(currentDate);
   }
   displayDate();
+
+
+  // Button to save 'time' and 'text' variables in local storage for later use on click.
+  saveButtonEl.on('click', function () {
+    var time = $(this).parent().attr('id');
+    var text = $(this).siblings('.description').val();
+    localStorage.setItem(time, text);
+ 
+  });
+
+  // Variable to format the current hour using a 24-hour clock.
+  var currentHour = dayjs().format('H');
+
+  // For loop that takes the time-block's hour in the form of an integer to compare to the currentHour variable integer.
+  // Then, it adds/removes the future, past, or present classes based on that comparison.
+  var elements = $('.time-block');
+  for (i = 0; i < elements.length; i++) {
+    var h = parseInt(elements[i].getAttribute('data-hour'));
+
+    if (currentHour < h) {
+      elements[i].classList.add('future');
+      elements[i].classList.remove('past');
+      elements[i].classList.remove('present');
+    } else if (h < currentHour) {
+      elements[i].classList.add('past');
+      elements[i].classList.remove('future');
+      elements[i].classList.remove('present');
+    } else if (h == currentHour) {
+      elements[i].classList.add('present');
+      elements[i].classList.remove('future');
+      elements[i].classList.remove('past');
+  } 
+  };
+
+  // Retrieves the stored data to use in the for loop.
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
+  
 });
 
 
